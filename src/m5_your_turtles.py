41457a7b3ce2e93a3b7a4304571c1a225c02dd67
pricear <pricear@rose-hulman.edu>
2019-05-22 19:38:04 -0400

    Work from the first face-to-face session

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Alexis Price.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,206 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+alexis = rg.SimpleTurtle('turtle')
+alexis.pen = rg.Pen('red', 5)
+alexis.speed = 30
+size = 300
+
+for k in range(13):
+
+    alexis.draw_square(30)
+
+    alexis.pen_up()
+    alexis.left(12)
+    alexis.forward(4)
+
+    alexis.pen_down()
+    size = size
+
+    alexis.pen_up()
+    alexis.left(12)
+    alexis.forward(4)
+
+    alexis.pen_down()
+    size = size
+
+
+    alexis.pen_up()
+    alexis.left(12)
+    alexis.forward(4)
+
+    alexis.pen_down()
+    size = size
+
+
+    alexis.pen_up()
+    alexis.left(12)
+    alexis.forward(4)
+
+    alexis.pen_down()
+    size = size
+
+
+    alexis.pen_up()
+    alexis.left(12)
+    alexis.forward(4)
+
+    alexis.pen_down()
+    size = size
+
+
+
+    alexis.pen_up()
+    alexis.left(24)
+    alexis.forward(10)
+
+    alexis.pen_down()
+    size = size
+
+
+    alexis.pen_up()
+    alexis.left(24)
+    alexis.forward(10)
+
+    alexis.pen_down()
+    size = size
+
+
+
+    alexis.pen_up()
+    alexis.left(24)
+    alexis.forward(10)
+
+    alexis.pen_down()
+    size = size
+
+
+
+    alexis.pen_up()
+    alexis.left(24)
+    alexis.forward(10)
+
+    alexis.pen_down()
+    size = size
+
+
+    alexis.pen_up()
+    alexis.left(30)
+    alexis.forward(15)
+
+    alexis.pen_down()
+    size = size
+
+
+    alexis.pen_up()
+    alexis.left(30)
+    alexis.forward(15)
+
+    alexis.pen_down()
+    size = size
+
+    alexis.pen_up()
+    alexis.left(30)
+    alexis.forward(15)
+
+    alexis.pen_down()
+    size = size
+
+window = rg.TurtleWindow()
+lucas = rg.SimpleTurtle('turtle')
+lucas.pen = rg.Pen('purple', 5)
+lucas.speed = 30
+size = 500
+
+for k in range(13):
+
+    lucas.draw_square(90)
+
+    lucas.pen_up()
+    lucas.left(12)
+    lucas.forward(4)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(12)
+    lucas.forward(4)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(12)
+    lucas.forward(4)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(24)
+    lucas.forward(10)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(24)
+    lucas.forward(10)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(24)
+    lucas.forward(10)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(24)
+    lucas.forward(10)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(24)
+    lucas.forward(10)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(24)
+    lucas.forward(10)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(30)
+    lucas.forward(15)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(30)
+    lucas.forward(15)
+
+    lucas.pen_down()
+    size = size
+
+    lucas.pen_up()
+    lucas.left(30)
+    lucas.forward(15)
+
+    lucas.pen_down()
+    size = size
+window.close_on_mouse_click()
