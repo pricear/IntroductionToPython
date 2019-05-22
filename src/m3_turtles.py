c3aae28b0046e03ca692cd262b45debd801f30ff
pricear <pricear@rose-hulman.edu>
2019-05-22 18:46:10 -0400

    Work from the first face-to-face session

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -10,11 +10,11 @@ Also:
   -- ASSIGNING a VALUE to a NAME (VARIABLE).
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Alexis Price.
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -77,7 +77,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -88,10 +88,13 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+dave.left(75)
+dave.forward(90)
+matt.right(89)
+matt.forward(73)
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,7 +112,11 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
-
+jason=rg.SimpleTurtle('turtle')
+jason.pen=rg.Pen('blue',10)
+jason.forward(120)
+jason.left(13)
+jason.backward(80)
 ########################################################################
 #
 # TODO: 5.
